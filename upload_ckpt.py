6615c30dae133cd935ed95f9d2b7894723e2f2c3
ngocdaobao <dbaongoc2312@gmail.com>
2026-08-09 00:51:41 +0700

    edit path

--- upload_ckpt.py
+++ upload_ckpt.py
@@ -3,7 +3,7 @@ from huggingface_hub import HfApi
 api = HfApi()
 
 api.upload_folder(
-    folder_path="runs/robomme/robomme/zoo_n1d6_pool_2short_mem/checkpoint-60000",
+    folder_path="runs/robomme/zoo_n1d6_pool_2short_mem/checkpoint-60000",
     repo_id="baongocdao/zoo_n1d6_pool_short_and_long_mem",
     repo_type="model",
 )
