64a48cfad91e7f659da91dd3b33c163f66c19a7e
ngocdaobao <dbaongoc2312@gmail.com>
2026-08-13 14:05:05 +0700

    change output folder

--- upload_ckpt.py
+++ upload_ckpt.py
@@ -3,7 +3,7 @@ from huggingface_hub import HfApi
 api = HfApi()
 
 api.upload_folder(
-    folder_path="runs/robomme/zoo_n1d6_fix_dataloader_11_history/checkpoint-60000",
-    repo_id="baongocdao/zoo_n1d6_fix_dataloader_11_history",
+    folder_path="runs/robomme/zoo_n1d6_fix_dataloader_lr_5e_5/checkpoint-60000",
+    repo_id="baongocdao/zoo_n1d6_fix_dataloader_lr_5e_5",
     repo_type="model",
 )
